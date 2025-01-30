6b166159fbb1a2f21dd268c37261777b8ce9278b
Jota <jota.zehering@gmail.com>
2025-01-30 03:07:36 -0300

    add js particles e consertos no css

---
+++ particles.js
@@ -0,0 +1,11 @@
+particlesJS("particles-js", {
+    particles: {
+        number: { value : 100 },
+        shape: { type: "circle" },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        move: { speed: 2 },
+        line_linked: {enable: true, distance: 150, color: "#ffffff"}
+    }
+}
+);
